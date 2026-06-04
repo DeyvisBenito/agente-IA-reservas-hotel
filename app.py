11fc53f41eec470e2820a1a96ff8cc2c7ea81a2b
deyvisbenito <deyvisbenito@gmail.com>
2026-06-03 23:16:17 -0600

    Agregando os y manejando host para render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -21,5 +22,5 @@ def chat():
     return jsonify(result)
 
 if __name__ == '__main__':
-    print('Servidor corriendo en http://localhost:5000')
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
